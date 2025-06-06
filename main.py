d73727e4a2ac4dcd9923e16b390791acea1bbcc1
marsik585 <marsel07072011@gmail.com>
2025-06-06 21:02:52 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
+        super().__init__()
+         # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+         # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
